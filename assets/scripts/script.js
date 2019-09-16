e8d4292cf35c3d6240261239e15a8630ccde38c3
tangratannakra <stela.tasheva@gmail.com>
2019-09-16 16:34:55 +0300

    Adding JS ES6 thumbnail click functionality to the product gallery

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,3 +2,18 @@ const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 
 burger.addEventListener('click', () => menu.classList.toggle('menu-active'));
+
+//The Gallery Slider
+
+    //TO DO: left and righ arrow keybord navigation to be added
+        
+         //grabbing buttons and listeting for clicks
+         const nextBtn = document.querySelector('.next');
+         const prevBtn = document.querySelector('.prev');
+         const currentImage = document.querySelector('.current');
+         const imgs = document.querySelectorAll('.thumbs img');
+
+         imgs.forEach(img => img.addEventListener('click', (e) => currentImage.src = e.target.src))
+
+         //nextBtn.addEventListener('click', changeCurrent);
+        // prevBtn.addEventListener('click', changeCurrent);
