24d827378baf4642e42fa3fbc3fc7aacb42dabee
tangratannakra <stela.tasheva@gmail.com>
2019-09-16 17:25:29 +0300

    adding prev/next functionality to the gallery

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,16 +4,22 @@ const menu = document.querySelector('.menu');
 burger.addEventListener('click', () => menu.classList.toggle('menu-active'));
 
 //The Gallery Slider
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+const currentImage = document.querySelector('.current');         
+const imgs = document.querySelectorAll('.thumbs img');
 
-    //TO DO: left and righ arrow keybord navigation to be added
+imgs.forEach(img => img.addEventListener('click', (e) => currentImage.src = e.target.src))
+currentImage.src = imgs[0].src;
         
-         //grabbing buttons and listeting for clicks
-         const nextBtn = document.querySelector('.next');
-         const prevBtn = document.querySelector('.prev');
-         const currentImage = document.querySelector('.current');
-         const imgs = document.querySelectorAll('.thumbs img');
+let i = imgs.length;
 
-         imgs.forEach(img => img.addEventListener('click', (e) => currentImage.src = e.target.src))
-
-         //nextBtn.addEventListener('click', changeCurrent);
-        // prevBtn.addEventListener('click', changeCurrent);
+nextBtn.onclick = () => {
+    i = (i < imgs.length-1)?(i = i+1):(i=0);
+    currentImage.src = imgs[i].src;
+         }
+       
+    prevBtn.onclick = () => {
+        i = (i < imgs.length + 1 && i > 0 )?(i =i-1):(i=imgs.length);
+        currentImage.src = imgs[i].src;
+         }
