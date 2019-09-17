fd93dc776d4e53af4e0d8dd289108b4af03fb9e2
tangratannakra <stela.tasheva@gmail.com>
2019-09-17 13:44:50 +0300

    Adding Clear form functionality and increment/decrement quantity

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('.menu');
 
 burger.addEventListener('click', () => menu.classList.toggle('menu-active'));
 
-//The Gallery Slider
+//The Gallery
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
 const currentImage = document.querySelector('.current');         
@@ -16,6 +16,15 @@ let i = imgs.length;
 //console.log(i);
 
 nextBtn.onclick = () => {
+    //Problemyt e, `e pyrwiq image e imgs[0] oshtee predi da se cykne next.
+    //Kato se cykne next zapo4wa da broi ot 0 (i=0); i defakto izobravenieto se powtarq
+    //dwa pyti imgs[0], imgs[0]. 
+    //Ako setna i=1, preska4a pyrwoto izobrajenie na sledwashtiq cikyl;
+    //opitah se da napisha if {} else {} i podobni, ama neshto pak ne mi se nawryzwa 
+    //logikata;
+    //towa se sluchwa samo na pyrwi cikyl.
+
+    // KAK DA IZKLJUCHA PARVOTO IZOBRAVENIE OT QUE-TO, NO SAMO WEDNYJ?!
     i = (i < imgs.length-1)?(i = i+1):(i=0);
     currentImage.src = imgs[i].src;  
     }
@@ -23,4 +32,36 @@ nextBtn.onclick = () => {
 prevBtn.onclick = () => {
     i = (i < imgs.length && i > 0 )?(i = i-1):(i=imgs.length-1);
     currentImage.src = imgs[i].src;
-    }     
+    }
+
+
+// Clear Selection
+const clear = document.querySelector('.clear-selection');
+const color = document.querySelector('.color');
+const size = document.querySelector('.size');
+const quantity = document.querySelector('.quantity-value');
+
+clear.onclick = () => {
+    color.value = 'red';
+    size.value = 'default';
+    quantity.value = '1';
+}
+
+//increment/decrement quantity
+const plusBtn = document.querySelector('.plus');
+const minusBtn = document.querySelector('.minus');
+
+let x = quantity.value;
+x = parseInt(x);
+
+plusBtn.onclick = (e) => {
+    e.preventDefault();
+    x = x + 1;
+    quantity.value = x;
+} 
+
+minusBtn.onclick = (e) => {
+    e.preventDefault();
+    x = x > 1?x - 1:x = 1;
+    quantity.value = x;
+}
